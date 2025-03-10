a62111ac8afccc2c4a2f7ce2c19b8aac306f2ad2
ibromr <omerogluibrahim@gmail.com>
2025-03-10 13:07:35 +1100

    Week03 workshop notes

---
+++ client1.py
@@ -0,0 +1,27 @@
+import socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+## Server info
+#client_socket.connect(("localhost", 12345))
+server_ip = 'localhost'
+server_port= 54323
+
+## Connect to the server
+client_socket.connect((server_ip, server_port))
+
+## Receiving incoming message
+message = client_socket.recv(1024)
+print("Message from server:", message.decode())
+
+
+# Send a custom message to the server
+custom_message = "Hello, server!"
+client_socket.sendall(custom_message.encode())
+
+# Receiving the response from the server
+response = client_socket.recv(1024)
+print("Response from server:", response.decode())
+
+
+# Closing the connection
+client_socket.close()
--- server.py
+++ server.py
@@ -1,9 +1,40 @@
 import socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("localhost", 12345))
+
+## Preperation for receviving incoming connection starts
+listen_ip = '0.0.0.0' # 10.125.12.37
+listen_port = 54325
+
+#server_socket.bind(("localhost", 12345))
+server_socket.bind((listen_ip, listen_port))
 server_socket.listen()
-print("Server is listening on port 12345...")
-conn, addr = server_socket.accept()
-print(f"Connection established with {addr}")
-conn.sendall(b"Hello from server!")
-conn.close()
+
+#print("Server is listening on port 12345...")
+print(f"Server is listening on port {listen_port}")
+## Preperation for receviving incoming connection done
+
+while True:
+    ## Establish connection wih client
+    conn, addr = server_socket.accept()
+    print(f"Connection established with {addr}")
+
+    ## Send message to the client
+    #conn.sendall(b"Hello from server!")
+    message = "Hello from server!"
+    conn.sendall(message.encode())
+
+     # Receive the client request
+    client_request = conn.recv(1024).decode()
+    print(f"Received request from client: {client_request}")
+
+    # Process the request and send a custom message
+    if client_request == "hello":
+        message = "Hello from server!"
+    elif client_request == "bye":
+        message = "Goodbye from server!"
+    else:
+        message = "Invalid request"
+
+
+    # Closing the connection
+    conn.close()
