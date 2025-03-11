bbcb74a7ae27638fb6373682c4d7f27f3149322d
ibromr <omerogluibrahim@gmail.com>
2025-03-11 17:55:56 +1100

    Configured sending messages in terminal between server and client

--- client1.py
+++ client1.py
@@ -9,19 +9,24 @@ server_port= 54323
 ## Connect to the server
 client_socket.connect((server_ip, server_port))
 
-## Receiving incoming message
-message = client_socket.recv(1024)
-print("Message from server:", message.decode())
+while True:
+    ## Receiving incoming message
+    message = client_socket.recv(1024)
+    print("Message from server:", message.decode())
 
+    # input message and send it to the server
+    msg = input("Enter message: ")
+    client_socket.send(msg.encode("utf-8")[:1024])
 
-# Send a custom message to the server
-custom_message = "Hello, server!"
-client_socket.sendall(custom_message.encode())
+    # receive message from the server
+    response = client_socket.recv(1024)
+    response = response.decode("utf-8")
 
-# Receiving the response from the server
-response = client_socket.recv(1024)
-print("Response from server:", response.decode())
+    # if server sent us "closed" in the payload, we break out of the loop and close our socket
+    if response.lower() == "closed":
+        break
 
+    print(f"Received: {response}")
 
 # Closing the connection
 client_socket.close()
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ## Preperation for receviving incoming connection starts
 listen_ip = '0.0.0.0' # 10.125.12.37
-listen_port = 54325
+listen_port = 54323
 
 #server_socket.bind(("localhost", 12345))
 server_socket.bind((listen_ip, listen_port))
@@ -22,19 +22,74 @@ while True:
     #conn.sendall(b"Hello from server!")
     message = "Hello from server!"
     conn.sendall(message.encode())
+    
 
      # Receive the client request
     client_request = conn.recv(1024).decode()
     print(f"Received request from client: {client_request}")
+    
+    
+    #client_request = conn.recv(1024).decode("utf-8") #convert bytes to string
 
-    # Process the request and send a custom message
+    # input message and send it to the client1
+    msg = input("Enter message: ")
+    conn.sendall(msg.encode("utf-8")[:1024])
+    
+    
+    # if we receive "close" from the client, then we break
+    # out of the loop and close the conneciton
+    if client_request.lower() == "close":
+        # send response to the client which acknowledges that the
+        # connection should be closed and break out of the loop
+        client_request.send("closed".encode("utf-8"))
+        break
+
+    print(f"Received: {client_request}")
+    
+    
+
+# Closing the connection
+conn.close()
+
+
+
+''''
+It working but you need to refine looping it sends the same message from client to server always: 
+
+ibromr@ibromr5520:~/coding/COMP3100DistributedSystems$ python3 server.py
+Server is listening on port 54323
+Connection established with ('127.0.0.1', 43986)
+Received request from client: heyo  >>>> 1. time
+Enter message: yoww
+Received: heyo               >>>>>>>>  2. times received 
+
+
+ibromr@ibromr5520:~/coding/COMP3100DistributedSystems$ python3 client1.py 
+Message from server: Hello from server!
+Enter message: heyo
+Received: yoww
+
+
+
+'''
+
+
+
+
+
+
+''''
+ # Process the request and send a custom message
     if client_request == "hello":
         message = "Hello from server!"
+        conn.send(message.encode())
+        
+        
     elif client_request == "bye":
         message = "Goodbye from server!"
-    else:
+        conn.send(message.encode())
+
+  else:
         message = "Invalid request"
-
-
-    # Closing the connection
-    conn.close()
+        conn.send(message.encode())  
+'''
